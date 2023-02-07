7b1ad233ffdf00f22502a0a9c3ea103a152802c6
Kafilat <kafilatadewumi61@gmail.com>
2023-02-07 08:57:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      <section className="section-one bg-[#131415] h-screen px-4">
+        <h1 className="text-white text-5xl font-extrabold mb-8 pt-20">
+          A <span className="text-focus">turn-key</span> approach to continuous
+          user discovery
+        </h1>
+        <h4 className="text-[#839799] font-semibold">
+          You've got a lot to take on so we take user research off your plate.
+        </h4>
+        <h2>How we work</h2>
+
+      </section>
+    </div>
+  );
+}
+
+export default App
